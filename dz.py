db04d2bc68aa9530b306dd918820ec74c2dd0a62
squ1shy27 <rybalkin.kirya@mail.ru>
2024-11-28 23:04:16 +0300

    окончательный вариант

--- dz.py
+++ dz.py
@@ -1,8 +1,10 @@
+
 def calculate_average(nums):
     total = sum(nums)
     count = len(nums)
     average = total / count
     return average
+
 
 nums = [10, 15, 20]
 result = calculate_average(nums)
